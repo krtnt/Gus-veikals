348435f83a1431b9fb981702393f1503db2334cb
krtnt <kristians1052r@gmail.com>
2025-05-03 17:14:06 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/produkti")
+def products():
+    conn = sqlite3.connect("gus.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM products
+        """
+    )
+    products = cur.fetchall()
+    conn.close()
+    return render_template("products.html", products=products)
+
+@app.route("/par-mums")
+def about():
+    return render_template("about.html")
+
+@app.route("/produkti/<int:product_id>")
+def products_show(product_id):
+    conn = sqlite3.connect("gus.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM products WHERE id = ?
+        """,
+        (product_id,),
+    )
+    product = cur.fetchone()
+    conn.close()
+    return render_template("products_show.html", product=product)
+
+if __name__ == "__main__":
+    app.run(debug=True)
