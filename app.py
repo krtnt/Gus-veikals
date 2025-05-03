467270c3560e76540e3c07aff33b6873b9a3d754
krtnt <kristians1052r@gmail.com>
2025-05-03 19:21:30 +0300

    functionality finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -19,7 +19,6 @@ def products():
     )
     products = cur.fetchall()
     conn.close()
-    print(products[0]['name'])
     return render_template("products.html", products=products)
 
 @app.route("/produkti/<int:product_id>")
@@ -34,8 +33,65 @@ def products_show(product_id):
         (product_id,),
     )
     product = cur.fetchone()
+
+    cur.execute(
+        """
+        SELECT * FROM mainIngridient WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    ingridient = cur.fetchone()
+
+    cur.execute(
+        """
+        SELECT * FROM allergens WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    allergen = cur.fetchone()
+
+    cur.execute(
+        """
+        SELECT * FROM toxicity WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    toxicity = cur.fetchone()
     conn.close()
-    return render_template("products_show.html", product=product)
+    return render_template("products_show.html", product=product, ingridient=ingridient, allergen=allergen, toxicity=toxicity)
+
+@app.route("/produkti/<int:product_id>/delete", methods=['POST'])
+def delete_products(product_id):
+    conn = sqlite3.connect("gus.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(
+        """
+        DELETE FROM mainIngridient WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    cur.execute(
+        """
+        DELETE FROM allergens WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    cur.execute(
+        """
+        DELETE FROM toxicity WHERE product_id = ?
+        """,
+        (product_id,),
+    )
+    cur.execute(
+        """
+        DELETE FROM products WHERE id = ?
+        """,
+        (product_id,),
+    )
+    conn.commit()
+    conn.close()
+    return redirect("/produkti")
 
 if __name__ == "__main__":
     app.run(debug=True)
